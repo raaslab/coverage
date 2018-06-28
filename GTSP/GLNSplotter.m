21401a3a8b92e98b39985c596d7a91114ef96244
Kevin Li Yu <klyu@vt.edu>
2018-06-28 12:40:30 -0400

    changed GLNSplotter so it plots the polygons correctly/added and example output picture

--- GTSP/GLNSplotter.m
+++ GTSP/GLNSplotter.m
@@ -3,9 +3,32 @@ close all
 load rando1.mat
 GLNSSolution = [12, 7, 13, 25]
 
-
+v_Cluster = cell2mat(v_Cluster);
 GLNSSolution = fliplr(GLNSSolution);
 GLNSSolutionOriginalPoints = ceil(GLNSSolution./numBatteryLevels);
+
+orig_V_Cluster = zeros([numPointsInit, 1]);
+for i = 1:numPointsInit
+    orig_V_Cluster(i) = v_Cluster(i*numBatteryLevels);
+end
+
+GLNSSolutionWithAllPoints = GLNSSolutionOriginalPoints;
+counter = 1;
+for i = 1:length(GLNSSolutionOriginalPoints)
+    if GLNSSolutionOriginalPoints(i) > numPointsInit
+        counter = counter+1;;
+    else
+        clusterToFind = orig_V_Cluster(GLNSSolutionOriginalPoints(i));
+        tempNewData = find(orig_V_Cluster == clusterToFind);
+        if tempNewData(1) ~= GLNSSolutionOriginalPoints(i)
+            GLNSSolutionWithAllPoints = [GLNSSolutionWithAllPoints(1:counter), tempNewData(1), GLNSSolutionWithAllPoints(counter+1:end)];
+            counter = counter+2;
+        else
+            GLNSSolutionWithAllPoints = [GLNSSolutionWithAllPoints(1:counter), tempNewData(2), GLNSSolutionWithAllPoints(counter+1:end)];
+            counter = counter+2;
+        end
+    end
+end
 
 GLNSx = zeros(1,numel(x1)/2);
 GLNSy = zeros(1,numel(y1)/2);
@@ -18,7 +41,8 @@ for a = 1:(numPointsInit)+1
         GLNSy(a) = y1(a-1);
     end
 end
-
+GLNSx = circshift(GLNSx, -1);
+GLNSy = circshift(GLNSy, -1);
 % GLNSx(end+1) = 0;
 % GLNSy(end+1) = 0;
 GLNSg = digraph;
@@ -27,14 +51,14 @@ figure(1)
 plot(GLNSx, GLNSy,'.')
 axis equal
 title('Initial Graph Without Edge Costs Edges are Euclidean Distance Between Points')
-% S2 = zeros(1,numel(GLNSx)-1);
-% T2 = S2;
-% for a = 1:numel(GLNSx)
-%     S2(a) = a;
-%     T2(a) = a+1;
-% end
-%
-% GLNSg = addedge(GLNSg,S2,T2);
+S2 = zeros(1,numel(GLNSx)-1);
+T2 = S2;
+for a = 1:numel(GLNSx)-1
+    S2(a) = GLNSSolutionWithAllPoints(a);
+    T2(a) = GLNSSolutionWithAllPoints(a+1);
+end
+
+GLNSg = addedge(GLNSg,S2,T2);
 
 S3 = [];
 S4 = S3;
@@ -54,6 +78,10 @@ for a = 1:(numPointsInit/2)
         typeChecker = v_Type(GLNSSolution(a),GLNSSolution(a+1));
         if typeChecker == 1
             S3(end+1) = a;
+            %             find(v_Cluster == a);
+            %             T3(end+1) = ;
+            %             S3(end+1) = ;
+            
             T3(end+1) = a+1;
         elseif typeChecker == 2
             S4(end+1) = a;
@@ -82,9 +110,9 @@ for i = 1:numel(GLNSx)
     end
 end
 
-GLNSg = addedge(GLNSg,S8,T8);
+% GLNSg = addedge(GLNSg,S8,T8);
 figure(2)
-GLNSPlot = plot(GLNSg,'XData',GLNSx,'YData',GLNSy, 'LineWidth',4, 'EdgeColor', 'w');
+GLNSPlot = plot(GLNSg,'XData',GLNSx,'YData',GLNSy, 'LineWidth',4, 'EdgeColor', 'b');
 axis equal
 
 
