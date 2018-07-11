031eced13e8992a14f4eab7cdb517a93ba3b547e
klyu-ironman <klyu@vt.edu>
2018-07-11 00:09:33 -0400

    need to finish polygonCreater.m

--- GTSP/polygonCreater.m
+++ GTSP/polygonCreater.m
@@ -1,5 +1,19 @@
-xlimit = [3 13];
-ylimit = [2  8];
-xbox = xlimit([1 1 2 2 1]);
-ybox = ylimit([1 2 2 1 1]);
-mapshow(xbox,ybox,'DisplayType','polygon','LineStyle','none')
+% polygonCreater.m
+
+numberOfPolygons = 5;
+x1 = Inf([1,numberOfPolygons]);
+y1 = Inf([1,numberOfPolygons]);
+x2 = Inf([1,numberOfPolygons]);
+y2 = Inf([1,numberOfPolygons]);
+i = 1;
+
+while i < numberOfPolygons
+x1(i) = randi(100,1);
+y1(i) = randi(100,1);
+x2(i) = randi(100,1);
+y2(i) = randi(100,1);
+
+% check if lines intersect
+i = i+1;
+
+end
