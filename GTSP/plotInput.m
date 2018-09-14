185df9345b29532966701058c41e3465d57d86c1
klyu-ironman <klyu@vt.edu>
2018-09-13 23:21:42 -0400

    finished creating boxes around input cells

--- GTSP/plotInput.m
+++ GTSP/plotInput.m
@@ -1,3 +1,4 @@
 % plots the input .txt file
-
-plotTXT('/home/klyu/lab/coverageWork/coverage/GTSP/inputs/costVSbudget1.txt')
+close all
+% plotTXT('/home/klyu/lab/coverageWork/coverage/GTSP/inputs/costVSbudget1.txt')
+plotTXT('D:\Documents\User_Created\School\School_Virginia_tech\Lab_work\coverage\GTSP\inputs\costVSbudget1.txt')
