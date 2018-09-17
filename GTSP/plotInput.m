00e4da60769bb57ec6a437eaeb9eb4fafff77c65
klyu-ironman <klyu@vt.edu>
2018-09-17 15:22:08 -0400

    added baselineMethod.m/need to get tour cost from GLNS

--- GTSP/plotInput.m
+++ GTSP/plotInput.m
@@ -1,4 +1,4 @@
 % plots the input .txt file
 close all
-plotTXT('/home/klyu/lab/coverageWork/coverage/GTSP/inputs/fieldExperiments/exampleFigureBC.txt')
+plotTXT('inputs/fieldExperiments/exampleFigureBC4.txt')
 % plotTXT('D:\Documents\User_Created\School\School_Virginia_tech\Lab_work\coverage\GTSP\inputs\costVSbudget1.txt')
